641ebcd1246792cf7b22148c56d510253c37b1ab
seunghabaek <slug0326@naver.com>
2022-06-23 00:27:43 +0900

    server side rendering

--- nextjs-intro/README.md
+++ nextjs-intro/README.md
@@ -7,3 +7,7 @@
 ### global.css
 
 : global.css에 css를 추가하고 (frame 같은 것) `_app.js`에서 읽어들이면 모든 page에 적용됨.
+
+### getServerSideProps
+
+: 이 함수를 쓰게 되면 서버에서만 동작하는 함수가 된다. 따라서 여기서 api call을 하고 이를 props로 front에 넘겨주는 방식을 취할 수 있다.
--- nextjs-intro/pages/index.js
+++ nextjs-intro/pages/index.js
@@ -4,25 +4,48 @@ import Seo from "../components/Seo";
 
 const API_KEY = "7f30c9c283aedcd16cc867379c201a05";
 
-export default function Home(){
-  const [movies, setMovies] = useState([]);
-  useEffect(() => {
-    (async () => {
-      const {results} = await (await fetch(
-        `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`
-        )).json();
-        setMovies(results);
-    })();
-  }, []);
+export default function Home({results}){
   return (
-    <div>
+    <div className="container">
     <Seo title="Home"></Seo>
-      {!movies && <h4>Loading...</h4>} 
-      {movies?.map((movie) => (
-        <div key = {movie.id}>
+      {results?.map((movie) => (
+        <div className="movie" key = {movie.id}>
+          <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} />
           <h4>{movie.original_title}</h4>
         </div>
       ))}
+      <style jsx>{`
+        .container {
+          display: grid;
+          grid-template-columns: 1fr 1fr;
+          padding: 20px;
+          gap: 20px;
+        }
+        .movie img {
+          max-width: 100%;
+          border-radius: 12px;
+          transition: transform 0.2s ease-in-out;
+          box-shadow: rgba(0, 0, 0, 0.1) 0px 4px 12px;
+        }
+        .movie:hover img {
+          transform: scale(1.05) translateY(-10px);
+        }
+        .movie h4 {
+          font-size: 18px;
+          text-align: center;
+        }
+      `}</style>
     </div>
   );
+}
+// server에서만 동작하는 코드
+export async function getServerSideProps() {
+  const { results } = await (await fetch(
+    `http://localhost:3000/api/movies`
+  )).json();
+  return {
+    props: {
+      results,
+    },
+  };
 }
